d8f7adb30ad260c8be98e596207475f8f650bbd6
Michael Benowitz <mwbenowitz@gmail.com>
2018-11-06 14:04:14 -0500

    SFR-174 Stream response objects to Kinesis
    This outputs a basic success/error object to a kinesis stream. From there
    it can be read into a database or otherwise handled

--- src/responseHandlers.js
+++ src/responseHandlers.js
@@ -1,7 +1,29 @@
-export const resultHandler = (handleResp) => {
-    // TODO Pass the results to SQS/Kinesis/postgresql
-    console.log(handleResp)
-    if (handleResp.status !== 200){
-        console.log("ERROR")
+import AWS from 'aws-sdk'
+
+AWS.config.update({
+    region: 'us-east-1',
+    logger: process.stdout
+})
+
+var customKinEndpoint
+if(process.env.AWS_KINESIS_ENDPOINT){
+    customKinEndpoint = {
+	endpoint: process.env.AWS_KINESIS_ENDPOINT
     }
+}
+
+const kinesis = new AWS.Kinesis(customKinEndpoint)
+
+export const resultHandler = (handleResp) => {
+    let outParams = {
+        Data: JSON.stringify(handleResp),
+        PartitionKey: process.env.AWS_KINESIS_STREAMID,
+        StreamName: process.env.AWS_KINESIS_STREAMNAME
+    }
+    let kinesisOut = kinesis.putRecord(outParams).promise()
+    kinesisOut.then((data) => {
+        console.log("Wrote Result to Kinesis stream")
+    }).catch((err) => {
+        console.log("FAILED TO PUT TO KINESIS")
+    })
 }
