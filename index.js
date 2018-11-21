6852781d6961ae7c1b20c77a727d5a4e66aad374
Michael Benowitz <mwbenowitz@gmail.com>
2018-11-21 10:24:22 -0500

    SFR-193 Move Accessibility Checker to standalone API
    Due to conflicting requirements of the Ace Report generator and the
    AWS Lambda environment, the actual work of generating those reports
    has been spun off into a separate API running on a t2.micro ec2
    instance. (These issues primarily relate to Ace's reliance on Chromium,
    which puts demands on package size, startup time and dependency
    resolution in the Lambda environment)

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import ResHandler from './src/responseHandlers'
 
 const fileNameRegex = /[0-9]+[.]{1}epub[.]{1}(?:no|)images/
 
-var records, zipData
+var records
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   records = event['Records']
   let resp
   if (!records || records.length < 1) {
@@ -19,17 +19,44 @@ exports.handler = (event, context, callback) => {
     ResHandler.resultHandler(resp)
     return callback(new Error('Kinesis stream failed or contained no records'))
   } else {
-    exports.parseRecords(records)
+    await exports.parseRecords(records)
     return callback(null, 'Successfully parsed records')
   }
 }
 
 exports.parseRecords = async (records) => {
   let results = records.map(exports.parseRecord)
-  await Promise.all(results)
-  results.forEach((res) => {
-    ResHandler.resultHandler(res)
+  return new Promise((resolve, reject) => {
+    Promise.all(results).then((responses) => {
+      responses.forEach((resp) => {
+        console.log(resp)
+        ResHandler.resultHandler(resp)
+      })
+      resolve()
+    })
   })
+}
+
+exports.runAccessCheck = async (zipData, itemID) => {
+  try{
+    let accessReport = await AccessibilityChecker.getAccessibilityReport(zipData)
+    accessReport['id'] = itemID
+    return {
+      "status": 200,
+      "code": "accessibility",
+      "message": "Created Accessibility Score",
+      "data": accessReport
+    }
+  } catch(err) {
+    return {
+      "status": 500,
+      "code": "Accessibility Report Error",
+      "data": {
+        "id": itemID
+      },
+      "message": err
+    }
+  }
 }
 
 exports.parseRecord = (record) => {
@@ -56,8 +83,9 @@ exports.parseRecord = (record) => {
       .then((response) => {
         Parser.epubExplode(fileName, itemID, updated, response)
         Parser.getBuffer(response.data).then((buffer) => {
-          zipData = buffer
           Parser.epubStore(fileName, itemID, updated, 'archive', buffer)
+          let reportBlock = exports.runAccessCheck(buffer, itemID)
+          return resolve(reportBlock)
         })
         .catch((error) => {
           return resolve({
@@ -68,26 +96,6 @@ exports.parseRecord = (record) => {
             },
             "message": error
           })
-        }).finally(async () => {
-          try{
-            let accessReport = await AccessibilityChecker.runAccessibilityReport(zipData)
-            accessReport['id'] = itemID
-            return resolve({
-              "status": 200,
-              "code": "accessibility",
-              "message": "Created Accessibility Score",
-              "data": accessReport
-            })
-          } catch(err) {
-            return resolve({
-              "status": 500,
-              "code": "Accessibility Report Error",
-              "data": {
-                "id": itemID
-              },
-              "message": err
-            })
-          }
         })
       })
     })
--- src/accessibilityCheck.js
+++ src/accessibilityCheck.js
@@ -1,103 +1,16 @@
-import ace from '@daisy/ace'
-import fs from 'fs'
-import { resolve } from 'path'
+import axios from 'axios'
 
 const STARTING_SCORE = 10
 const SCORE_DIVISOR = 4
 
-exports.runAccessibilityReport = async (buf) => {
-  return new Promise(async (res, reject) => {
-    let tmpFile, report
-    try{
-      tmpFile = await exports.saveTmpFile(buf)
-    } catch(e) {
-      reject(e)
-      return
-    }
-
-    let aceOpts = {
-      cwd: __dirname,
-      outdir: null,
-      tmpdir: null,
-      verbose: false,
-      silent: true
-    }
-
-    try{
-      report = await ace(tmpFile, aceOpts)
-    } catch(e) {
-      reject(e)
-      return
-    }
-
-    let reportSummary = exports.parseReport(report)
-    res(reportSummary)
-
-  })
-}
-
-exports.saveTmpFile = (buf) => {
+exports.getAccessibilityReport = (buf) => {
   return new Promise((res, reject) => {
-    let tmpFile = './tmp/' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-    fs.writeFile(tmpFile, buf, (err) => {
-      if (err) reject(err)
-      let absPath = resolve(tmpFile)
-      res(absPath)
+    let jsonBuf = JSON.stringify(buf)
+    axios.post(process.env.SFR_ACCESSIBILITY_API, JSON.parse(jsonBuf)).then((response) => {
+      res(response.data)
+    }).catch((err) => {
+      reject(err)
+      return
     })
   })
-}
-
-exports.parseReport = (report) => {
-  let mainReport = report[1]
-  let assertions = mainReport['assertions']
-  let timeRun = mainReport['dct:date']
-  let aceVersion = mainReport['earl:assertedBy']['doap:release']['doap:revision']
-  let assertionOut = exports.parseAssertions(assertions)
-  let vioObj = {}
-  assertionOut['violations'].forEach((value, key) => {
-    vioObj[key] = value
-  })
-  return {
-    'json': mainReport,
-    'aceVersion': aceVersion,
-    'timestamp': timeRun,
-    'score': assertionOut['score'],
-    'violations': vioObj
-  }
-}
-
-exports.parseAssertions = (assertions) => {
-  let output = {
-    'score': 0,
-    'violations': new Map([
-      ['critial', 0],
-      ['serious', 0],
-      ['moderate', 0],
-      ['minor', 0]
-    ])
-  }
-  assertions.map((assertion) => {
-    let tests = assertion['assertions']
-    tests.map((test)=> {
-      let errType = test['earl:test']['earl:impact']
-      let newCount = output['violations'].get(errType)
-      newCount++
-      output['violations'].set(errType, newCount)
-    })
-  })
-
-  let score = exports.calculateScore(output['violations'])
-  if (score > 0) output['score'] = score
-  return output
-}
-
-exports.calculateScore = (violations) => {
-  let score = STARTING_SCORE
-  let i = 0
-  violations.forEach((value, key) => {
-    score -= value/(Math.pow(SCORE_DIVISOR, i))
-    i++
-  })
-
-  return score
 }
